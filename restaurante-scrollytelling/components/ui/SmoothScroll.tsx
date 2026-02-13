0c4582396f37a041df0924c4c5060dedcb5fad6b
Esteban Gamboa <81179481+estebanGamboa-12@users.noreply.github.com>
2026-02-13 14:24:33 +0100

    Create luxury scrollytelling experience with GSAP scenes

---
+++ restaurante-scrollytelling/components/ui/SmoothScroll.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { ReactNode, useEffect } from "react";
+import Lenis from "lenis";
+
+type SmoothScrollProps = {
+  children: ReactNode;
+};
+
+export default function SmoothScroll({ children }: SmoothScrollProps) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smoothWheel: true,
+      touchMultiplier: 1.2,
+    });
+
+    let frame = 0;
+    const raf = (time: number) => {
+      lenis.raf(time);
+      frame = requestAnimationFrame(raf);
+    };
+
+    frame = requestAnimationFrame(raf);
+
+    return () => {
+      cancelAnimationFrame(frame);
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
+}
